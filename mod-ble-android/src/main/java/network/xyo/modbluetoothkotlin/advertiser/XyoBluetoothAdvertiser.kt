cff4928c622fbafc8052c5eae6443b9a2a875bab
Carter Harrison <carterjharrison@gmail.com>
2019-01-04 10:23:04 -0800

    fix static id

--- mod-ble-android/src/main/java/network/xyo/modbluetoothkotlin/advertiser/XyoBluetoothAdvertiser.kt
+++ mod-ble-android/src/main/java/network/xyo/modbluetoothkotlin/advertiser/XyoBluetoothAdvertiser.kt
@@ -21,7 +21,7 @@ class XyoBluetoothAdvertiser (val id : ByteArray, private val advertiser: XYBlue
         val manData = advertiser.changeManufacturerData(id, false).await()
         if (manData.error != null) return@async manData.error
 
-        val manId = advertiser.changeManufacturerId(13, false).await()
+        val manId = advertiser.changeManufacturerId(id, false).await()
         if (manId.error != null) return@async manId.error
 
         val conResult = advertiser.changeContactable(true, false).await()
