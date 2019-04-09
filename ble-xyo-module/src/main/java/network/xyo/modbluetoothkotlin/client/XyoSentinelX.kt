565d3e2d34637070da0493ae1803453cf955cbe4
Bob Nies <bob@xyo.network>
2019-04-08 18:21:49 -0700

    Added spota and FW update functionality to XyoSentinelX

--- ble-xyo-module/src/main/java/network/xyo/modbluetoothkotlin/client/XyoSentinelX.kt
+++ ble-xyo-module/src/main/java/network/xyo/modbluetoothkotlin/client/XyoSentinelX.kt
@@ -5,36 +5,42 @@ import kotlinx.coroutines.*
 import network.xyo.ble.devices.XY4BluetoothDevice
 import network.xyo.ble.devices.XYBluetoothDevice
 import network.xyo.ble.devices.XYCreator
+import network.xyo.ble.firmware.XYBluetoothDeviceUpdate
+import network.xyo.ble.firmware.XYOtaFile
+import network.xyo.ble.firmware.XYOtaUpdate
 import network.xyo.ble.gatt.peripheral.XYBluetoothError
 import network.xyo.ble.gatt.peripheral.XYBluetoothResult
 import network.xyo.ble.scanner.XYScanResult
+import network.xyo.ble.services.dialog.SpotaService
 import network.xyo.ble.services.standard.BatteryService
 import network.xyo.ble.services.standard.DeviceInformationService
 import network.xyo.ble.services.xy4.PrimaryService
 import network.xyo.modbluetoothkotlin.XyoUuids
+import java.io.ByteArrayInputStream
 import java.nio.ByteBuffer
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.experimental.and
 
-open class XyoSentinelX(context: Context, scanResult: XYScanResult, hash : Int) : XyoBluetoothClient(context, scanResult, hash) {
+open class XyoSentinelX(context: Context, scanResult: XYScanResult, hash: Int) : XyoBluetoothClient(context, scanResult, hash) {
     private val sentinelListeners = HashMap<String, Listener>()
-    private var lastButtonPressTime : Long = 0
+    private var lastButtonPressTime: Long = 0
 
     //Keep as public
     val batteryService = BatteryService(this)
     val primary = PrimaryService(this)
     val deviceInformationService = DeviceInformationService(this)
+    val spotaService = SpotaService(this)
 
-    fun addButtonListener (key : String, listener : Listener) {
+    fun addButtonListener(key: String, listener: Listener) {
         sentinelListeners[key] = listener
     }
 
-    fun removeButtonListener (key: String) {
+    fun removeButtonListener(key: String) {
         sentinelListeners.remove(key)
     }
 
-    fun isClaimed () : Boolean {
+    fun isClaimed(): Boolean {
         val iBeaconData = scanResult?.scanRecord?.getManufacturerSpecificData(0x4c) ?: return true
 
         if (iBeaconData.size == 23) {
@@ -45,7 +51,7 @@ open class XyoSentinelX(context: Context, scanResult: XYScanResult, hash : Int) 
         return true
     }
 
-    private fun isButtonPressed (scanResult: XYScanResult) : Boolean {
+    private fun isButtonPressed(scanResult: XYScanResult): Boolean {
         val iBeaconData = scanResult.scanRecord?.getManufacturerSpecificData(0x4c) ?: return true
 
         if (iBeaconData.size == 23) {
@@ -83,7 +89,7 @@ open class XyoSentinelX(context: Context, scanResult: XYScanResult, hash : Int) 
      * @param newPassword The password to change on the remote device.
      * @return An XYBluetoothError if there was an issue writing the packet.
      */
-    fun changePassword (password: ByteArray, newPassword: ByteArray) : Deferred<XYBluetoothError?> {
+    fun changePassword(password: ByteArray, newPassword: ByteArray): Deferred<XYBluetoothError?> {
         val encoded = ByteBuffer.allocate(2 + password.size + newPassword.size)
                 .put((password.size + 1).toByte())
                 .put(password)
@@ -101,7 +107,7 @@ open class XyoSentinelX(context: Context, scanResult: XYScanResult, hash : Int) 
      * @param password The password of the device to so it can write the boundWitnessData
      * @return An XYBluetoothError if there was an issue writing the packet.
      */
-    fun changeBoundWitnessData (password: ByteArray, boundWitnessData: ByteArray) : Deferred<XYBluetoothError?> {
+    fun changeBoundWitnessData(password: ByteArray, boundWitnessData: ByteArray): Deferred<XYBluetoothError?> {
         val encoded = ByteBuffer.allocate(3 + password.size + boundWitnessData.size)
                 .put((password.size + 1).toByte())
                 .put(password)
@@ -112,7 +118,7 @@ open class XyoSentinelX(context: Context, scanResult: XYScanResult, hash : Int) 
         return chunkSend(encoded, XyoUuids.XYO_CHANGE_BW_DATA, XyoUuids.XYO_SERVICE, 4)
     }
 
-    fun getBoundWitnessData () : Deferred<XYBluetoothResult<ByteArray>> {
+    fun getBoundWitnessData(): Deferred<XYBluetoothResult<ByteArray>> {
         return findAndReadCharacteristicBytes(XyoUuids.XYO_SERVICE, XyoUuids.XYO_CHANGE_BW_DATA)
     }
 
@@ -126,7 +132,7 @@ open class XyoSentinelX(context: Context, scanResult: XYScanResult, hash : Int) 
         return findAndWriteCharacteristic(XyoUuids.XYO_SERVICE, XyoUuids.XYO_RESET_DEVICE, msg)
     }
 
-    fun getPublicKey () : Deferred<XYBluetoothResult<ByteArray>> {
+    fun getPublicKey(): Deferred<XYBluetoothResult<ByteArray>> {
         return findAndReadCharacteristicBytes(XyoUuids.XYO_SERVICE, XyoUuids.XYO_PUBLIC_KEY)
     }
 
@@ -156,13 +162,25 @@ open class XyoSentinelX(context: Context, scanResult: XYScanResult, hash : Int) 
         return@connection batteryService.level.get().await()
     }
 
+    /**
+     * Firmware Update
+     * @param fileByteArray the firmware as a ByteArray
+     * @param listener listener for progress, failed, completed
+     */
+    fun updateFirmware(fileByteArray: ByteArrayInputStream, listener: XYOtaUpdate.Listener) {
+        val otaFile = fileByteArray?.let { XYOtaFile.getByStream(it) }
+        val updater = XYBluetoothDeviceUpdate(spotaService, this, otaFile)
+        updater.addListener("SentinelXDevice", listener)
+        updater.start()
+    }
+
     companion object : XYCreator() {
 
         open class Listener {
-            open fun onButtonPressed () {}
+            open fun onButtonPressed() {}
         }
 
-        fun enable (enable : Boolean) {
+        fun enable(enable: Boolean) {
             if (enable) {
                 XyoBluetoothClient.xyoManufactorIdToCreator[0x01] = this
             } else {
