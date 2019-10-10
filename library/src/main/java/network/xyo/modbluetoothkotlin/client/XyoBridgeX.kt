73b4681b9b2548b4bb9180e6e21134db95e4bc39
Arie Trouw <arie.trouw@xy.company>
2019-10-10 16:06:03 -0700

    Fixed BW?

--- library/src/main/java/network/xyo/modbluetoothkotlin/client/XyoBridgeX.kt
+++ library/src/main/java/network/xyo/modbluetoothkotlin/client/XyoBridgeX.kt
@@ -1,6 +1,8 @@
 package network.xyo.modbluetoothkotlin.client
 
+import android.bluetooth.BluetoothDevice
 import android.content.Context
+import android.os.Build
 import network.xyo.ble.generic.devices.XYBluetoothDevice
 import network.xyo.ble.generic.devices.XYCreator
 import network.xyo.ble.generic.scanner.XYScanResult
@@ -8,8 +10,11 @@ import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
 @kotlin.ExperimentalUnsignedTypes
-open class XyoBridgeX(context: Context, scanResult: XYScanResult, hash: String) :
-        XyoBluetoothClient(context, scanResult, hash) {
+open class XyoBridgeX: XyoBluetoothClient {
+
+    constructor(context: Context, scanResult: XYScanResult, hash: String) : super(context, scanResult, hash)
+
+    constructor(context: Context, scanResult: XYScanResult, hash: String, transport: Int) : super(context, scanResult, hash, transport)
 
     companion object : XYCreator() {
 
@@ -29,7 +34,11 @@ open class XyoBridgeX(context: Context, scanResult: XYScanResult, hash: String) 
                         XYBluetoothDevice>
         ) {
             val hash = hashFromScanResult(scanResult)
-            val createdDevice = XyoBridgeX(context, scanResult, hash)
+            val createdDevice = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                XyoBridgeX(context, scanResult, hash, BluetoothDevice.TRANSPORT_LE)
+            } else {
+                XyoBridgeX(context, scanResult, hash)
+            }
             val foundDevice = foundDevices[hash]
             if (foundDevice != null) {
                 foundDevice.rssi = scanResult.rssi
